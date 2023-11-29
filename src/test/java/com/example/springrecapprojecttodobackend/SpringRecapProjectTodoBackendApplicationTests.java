7d96129384ce1175540b16f41a8770bc42992400
siqizheng <cn.siqizheng@gmail.com>
2023-11-29 14:16:19 +0100

    Initial commit

---
+++ src/test/java/com/example/springrecapprojecttodobackend/SpringRecapProjectTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrecapprojecttodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecapProjectTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
